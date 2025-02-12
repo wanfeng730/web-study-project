c9099424cf6144afb6d2bf17d574d7fccb7b781a
wanfeng730 <2580330397@qq.com>
2025-02-12 21:11:31 +0800

    feat: js start 1.update property 2.select table all line

--- doc-js/start_04_DOM.js
+++ doc-js/start_04_DOM.js
@@ -31,21 +31,35 @@ var v6 = document.documentElement;
 console.log(v6);
 
 
+var button1 = document.getElementById('button1');
+var title = document.getElementsByClassName('titleClass')[0];
+var desc = document.getElementsByClassName('descClass')[0];
+var a1 = document.getElementById("a1");
+
+// 修改元素内容，不识别html标签，会去除
+var text = title.innerText;
+console.log(text);
+text = text + new Date();
+title.innerText = text;
+
+// 修改元素内容，识别html标签（推荐使用）
+desc.innerHTML = new Date();
+
+// 修改元素属性
+a1.href = "http://www.baidu.com";
+
+// 修改表单元素属性：输入框内容
+var input1 = document.getElementById("input1");
+input1.value = "update-value";
+
+// 修改元素样式（行内样式）
+title.style.backgroundColor = 'blue';
 
 
 // 事件三要素：事件源、事件类型，事件处理函数
 // 按钮点击事件
-var button1 = document.getElementById('button1');
-var title = document.getElementsByClassName('titleClass')[0];
-var desc = document.getElementsByClassName('descClass')[0];
 button1.onclick = function(){
-    // 修改元素内容，不识别html标签，会去除
-    var text = title.innerText;
-    console.log(text);
-    text = text + new Date();
-    title.innerText = text;
-    // 修改元素内容，识别html标签（推荐使用）
-    desc.innerHTML = new Date();
-
-    // alert('button1点击事件');
+    alert('button1点击事件');
+    // this指向函数的调用者
+    console.log(this);
 }
