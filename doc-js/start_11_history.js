c94c784a0a12b43c005adf0cf0c86a0f6ae42a1c
wanfeng730 <2580330397@qq.com>
2025-02-15 19:44:35 +0800

    feat: js history

---
+++ doc-js/start_11_history.js
@@ -0,0 +1,16 @@
+var historyButton = document.getElementById('history')
+
+
+// history对象，包含用户访问过的url
+historyButton.addEventListener('click', function(e){
+    console.log(history);
+    
+})
+
+// // 前进一个历史记录
+// history.forward();
+
+// // 后退一个历史记录
+// history.back(); 
+
+console.log(history);
