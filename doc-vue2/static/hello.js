0471ca0d8813469b4fb8729d57571bf532c68a8d
wanfeng730 <2580330397@qq.com>
2025-03-08 17:12:15 +0800

    feat: vue 监视属性

--- doc-vue2/static/hello.js
+++ doc-vue2/static/hello.js
@@ -62,20 +62,29 @@ var vue = new Vue({
             console.log('fullName2 getter...');
             return this.firstName + this.lastName;
         }
+    },
+
+    // 监视属性
+    watch:{
+        lastName:{
+            // 设置为初始时候立即执行handler
+            immediate: true,
+            handler(newValue, oldValue){
+                console.log(`watch lastName handler newValue=${newValue} oldValue=${oldValue}`);
+            }
+        }
     }
 
 });
-// 构建vue对象后 页面上的DevTools插件已经可以生效
-console.log('Vue实例对象:', vue);
 
-// 获取vue实例中定义的数据
-console.log(vue.username);
-console.log(vue.unitCode);
+// // 获取vue实例中定义的数据
+// console.log(vue.username);
+// console.log(vue.unitCode);
 
 // 可以手动挂载元素，效果等同于Vue实例的el属性
 vue.$mount('#root')
 
-console.log(vue.$data);
+// console.log(vue.$data);
 
 
 let gobalTitle = 'GGG';
@@ -104,7 +113,14 @@ Object.defineProperty(data1, 'title', {
         gobalTitle = value;
     }
 })
-console.log(data1);
+// console.log(data1);
 
+
+// 监视属性简写
+vue.$watch('firstName', {
+    handler(newValue, oldValue){
+        console.log(`watch firstName ${newValue} ${oldValue}`);
+    }
+})
 
 
