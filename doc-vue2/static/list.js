eb48dd18952a40253d38f956cb3db395a7dedab3
wanfeng730 <2580330397@qq.com>
2025-03-21 01:19:38 +0800

    feat: vue 列表过滤

--- doc-vue2/static/list.js
+++ doc-vue2/static/list.js
@@ -4,23 +4,55 @@ const vue = new Vue({
         personList:[
             {
                 id: '001',
-                name: 'wanfeng',
+                name: '晚风',
                 age: 25
             },
             {
                 id: '002',
-                name: 'xiadie',
-                age: 23
+                name: '遐蝶',
+                age: 21
+            },
+            {
+                id: '003',
+                name: '万叶',
+                age: 26
             }
         ],
         archive:{
             archivalId: 'A001',
             title: '001档案',
             fileYear: 2025
-        }
+        },
+        searchNameContent: '',
+        filterPersonList: [],
+        searchIdContent: ''
     },
     methods:{
         
+    },
+    computed:{
+        // 计算属性实现过滤，定义过滤后的列表=全部数据中包含搜索内容的结果
+        // get调用时机：初次调用时、所以依赖的数据发生变化时
+        filterIdList: {
+            get(){
+                console.log('compute filterIdList');
+                return this.personList.filter((item) => {
+                    return item.id.indexOf(this.searchIdContent) != -1;
+                })
+            }
+        }
+    },
+    watch:{
+        // watch监视实现过滤，监听到搜索内容变化时修改显示过滤的列表
+        searchNameContent:{
+            immediate: true,
+            handler(value){
+                console.log('watch searchNameContent='+value);
+                this.filterPersonList = this.personList.filter((item) => {
+                    return item.name.indexOf(value) != -1;
+                });
+            }
+        }
     }
 })
 
