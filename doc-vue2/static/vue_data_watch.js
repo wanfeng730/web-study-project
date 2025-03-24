d1ace7cee767336ad1ac5c8c44764f4903b89c68
wanfeng730 <2580330397@qq.com>
2025-03-24 21:30:42 +0800

    feat: vue监视数组改变

--- doc-vue2/static/vue_data_watch.js
+++ doc-vue2/static/vue_data_watch.js
@@ -23,3 +23,47 @@ let archive = {
 
 const obs = new Observer(archive);
 console.log(obs);
+
+
+const vue = new Vue({
+    el: '#root',
+    data:{
+        archive:{
+            archivalId: 'A001',
+            title: 'A0001档案',
+            linkPaths: [
+                '/档案库',
+                '/专题库'
+            ]
+        }
+    },
+    methods:{
+        setType(){
+            console.log('set type...');
+            Vue.set(this.archive, 'type', 'Record');
+        },
+        updateLinkPaths(){
+            /**
+             Vue 将被侦听的数组的变更方法进行了包裹，所以它们也将会触发视图更新。这些被包裹过的方法包括：
+                push()
+                pop()
+                shift()
+                unshift()
+                splice()
+                sort()
+                reverse()
+             */
+            this.archive.linkPaths.push('/业务系统收集');
+        }
+    },
+    computed:{
+        
+    },
+    watch:{
+        
+    }
+});
+console.log(vue);
+
+// 给vue中增加数据要用响应式写法，追加数据只能往vue中的数据对象中加，不能在data中直接加入
+Vue.set(vue.archive, 'parentId', '0');
