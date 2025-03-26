c0648612eea3b3b0312ab837ecb0b145020e8ecd
wanfeng730 <2580330397@qq.com>
2025-03-26 22:26:20 +0800

    feat: vue表单提交、相关组件功能

--- doc-vue2/static/form.js
+++ doc-vue2/static/form.js
@@ -1,14 +1,28 @@
 var vue = new Vue({
     el: '#root',
     data:{
-        // data中初识的值对应到表单元素的value后可以设定默认值
-        username: '',
-        password: '',
-        sex: '2',
-        hobby: [],
-        area: 'jinhua',
-        desc: '默认信息',
-        agreeUserPaper: false,
-    }
+        formData:{
+            // data中初识的值对应到表单元素的value后可以设定默认值
+            username: '',
+            password: '',
+            dayCount: 23,
+            sex: '2',
+            hobby: [],
+            area: 'jinhua',
+            desc: '默认信息',
+            agreeUserPaper: false,
+        }
+    },
+    
+    methods:{
+        submitInfo(){
+            // 获取表单数据，转为json字符串格式
+            body = JSON.stringify(this.formData)
+            console.log(body);
+        }
+    },
+    
+    
+    
 });
 console.log(vue);
