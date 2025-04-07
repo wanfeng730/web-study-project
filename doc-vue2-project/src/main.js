3724476cb051f1d67aadf43d1020150d88a61677
wanfeng730 <2580330397@qq.com>
2025-04-08 00:52:56 +0800

    feat: vue runtime版

--- doc-vue2-project/src/main.js
+++ doc-vue2-project/src/main.js
@@ -1,3 +1,4 @@
+// 引入的是vue.runtime.xxx.js，这种vue是没法解析template配置的，只包含核心功能
 import Vue from 'vue'
 import App from './App.vue'
 
@@ -7,5 +8,10 @@ Vue.config.productionTip = false
 // 创建Vue实例对象
 new Vue({
   // 将App组件放入容器（el）中，渲染出来
-  render: h => h(App),
+  render: h => h(App)
+
+  // render函数的原型，createElementFunc是创建一个标签或组件的函数
+  // render(createElementFunc){
+  //   return createElementFunc(App)
+  // }
 }).$mount('#app')
