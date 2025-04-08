84be64dc9994a8edc1eb37195d1e62cf2267902c
wanfeng730 <2580330397@qq.com>
2025-04-08 21:59:12 +0800

    feat： vue脚手架配置项

--- doc-vue2-project/vue.config.js
+++ doc-vue2-project/vue.config.js
@@ -1,6 +1,25 @@
 const { defineConfig } = require('@vue/cli-service')
+
+// 配置vue脚手架的相关配置
 module.exports = defineConfig({
   transpileDependencies: true,
   // 关闭lint语法检查
-  lintOnSave: false
+  lintOnSave: false,
+
+  pages: {
+    index: {
+      // page 的入口
+      entry: 'src/main.js',
+      // 模板来源
+      template: 'public/index.html',
+      // 在 dist/index.html 的输出
+      filename: 'index.html',
+      // 当使用 title 选项时，
+      // template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
+      title: 'Vue页面Title配置内容',
+      // 在这个页面中包含的块，默认情况下会包含
+      // 提取出来的通用 chunk 和 vendor chunk。
+      chunks: ['chunk-vendors', 'chunk-common', 'index']
+    }
+  }
 })
