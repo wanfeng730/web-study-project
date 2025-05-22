b815d59e04e833c51d9c12082b8cc47416be54da
wanfeng730 <2580330397@qq.com>
2025-05-22 23:45:45 +0800

    feat: 路由 replace

--- doc-vue2-project/src/pages/Messages.vue
+++ doc-vue2-project/src/pages/Messages.vue
@@ -8,7 +8,8 @@
       <!-- 拼接参数方式跳转 -->
       <!-- <router-link active-class="active_message" :to="`/about/messages/detail?id=${item.id}&name=${item.name}`">{{ item.name }}</router-link> -->
       <!-- 携带对象参数方式跳转 -->
-      <router-link active-class="active_message" :to="{
+      <!-- 开启replace模式，即不保存历史记录，不可后退（实际场景：防止回退后重要的表单重复提交） -->
+      <router-link replace active-class="active_message" :to="{
         path: '/about/messages/detail',
         query:{
           id: item.id,
