cdab53f65739306dd30431dc6cfa605c3e8940fb
wanfeng730 <2580330397@qq.com>
2025-08-23 15:34:50 +0800

    feat: ts语法2

--- doc-ts/src/ts/start_01.ts
+++ doc-ts/src/ts/start_01.ts
@@ -33,7 +33,7 @@ bool2 = false;
  * unknown：未知类型，类型安全的any，可以随便赋值，但是不能将unknown变量赋值给特定类型的变量，不能对unknown变量调用方法
  * never: 无论什么值都不能存，通常用于永远不会执行结束或一定抛出异常的函数的返回值类型
  * void：空类型，通常用于指定函数返回值为空
- * tuple：元组
+ * tuple：元组，固定数量的数组
  * enum：枚举
  */
 let any1: any;
@@ -62,3 +62,63 @@ function func3():void{
   console.log('func3');
 }
 
+// 声明对象的结构 必须包含string类型的name，number类型的age可以没有
+let obj1: {
+  name: string, 
+  age?: number,
+  [key: string]: any   //索引签名，表示可以在对象中追加任意string的key，任意类型的值
+}
+obj1 = {name: 'wanfeng', age: 18}
+
+//声明函数的参数类型、返回值类型
+let func4: (a: string, b: number) => number;
+func4 = function (a, b) {
+  return 0;
+}
+
+// 声明数组的类型
+let arr1: string[]
+let arr2: Array<string> //泛型格式
+
+// 声明元组(固定数量的数组)
+let tup1: [string, number, boolean]
+let tup2: [string, ...number[]];
+tup1 = ['wanfeng', 28, true];
+tup2 = ['wanfeng', 24, 56, 7, 9];
+
+//声明枚举，默认的值为顺序数字
+enum Status{
+  DRAFT = '草稿',
+  PROCESS = '进行中',
+  DONE = '完成'
+};
+
+let status1 = Status.DONE;
+
+// 声明某种类型的别名
+type T1 = number;
+type T2 = number | boolean;
+type Gender = '男' | '女';  //可以用于定义枚举
+
+// 交叉类型，拼接两种对象类型
+type Area = {
+  height: number,
+  weight: number
+}
+type Address = {
+  num: number,
+  room: string
+}
+type House = Area & Address;
+const house1: House = {
+  height: 100,
+  weight: 100,
+  num: 3,
+  room: 'wanfeng'
+}
+
+//特殊情况：使用类型声明函数返回值类型时 TypeSxcript并不会严格要求函数返回空
+type LogFunc = () => void;
+const func5: LogFunc = () => {
+  return 666;
+}
